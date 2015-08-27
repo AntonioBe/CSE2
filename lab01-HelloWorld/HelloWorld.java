0a0bea33684382072faa8f8a567690bf5065817e
anb219 <anb219@lehigh.edu>
2015-08-27 15:59:44 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,24 @@
+//antonio Berjillos G.
+//cse02 Lab01
+//Hello World Java Program
+// Compile the java program Helloworld.java
+//  Staert running the program Hello World
+
+
+// Frirst, define a public class
+public class HelloWorld{
+
+    //We add a main method
+    public static void main (String[] args) {
+    
+        //write the statement 
+        System.out.println("################################");
+        System.out.println("#############################");
+        System.out.println("###");
+        System.out.println("   Hello World 2015 !!");
+        System.out.println("######");
+        System.out.println("################");
+        System.out.println("###########################################");
+        System.out.println("testing");
+    }
+}
